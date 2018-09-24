11831310e67602f822243ca68b53b1d21d6c29fa
Emilio <you@example.com>
2018-09-24 23:09:07 +0200

    Euler method for initial value problem of the derivate with  EDO system fun

--- e.m
+++ e.m
@@ -1,4 +1,6 @@
+%Initial time%
 t = 0;
+%Initial value in t =0%
 u0 = [2;3];
-y = fun(t,u0);
+%Just cheking... y = fun(t,u0);%
 euler(@(t,u)fun(t,u0), 200, 0, 7, u0)
--- fun.m
+++ fun.m
@@ -1,6 +1,5 @@
 function y=fun(t,u)
-%FUN Summary of this function goes here
-%   Detailed explanation goes here
+%EDO  system
     y=[-2, 1;1,-1]*u+[(2*sin(t));2*(cos(t)-sin(t))];
 end
 
